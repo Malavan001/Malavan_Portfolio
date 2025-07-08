817b9f6ec76845d9e1ecc282e8a24a2f00128853
Malavan001 <malavan235@gmail.com>
2025-07-08 17:53:11 +0530

    Vit config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+import react from "@vitejs/plugin-react";
 
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: env.VITE_BASE_URL || '/',
+    base: "/",
+    build: {
+      chunkSizeWarningLimit: 800,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom"],
+            mui: ["@mui/material", "@mui/icons-material"],
+            // Add more only if they're installed and used
+          },
+        },
+      },
+    },
   };
 });
